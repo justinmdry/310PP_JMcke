3a4c3dbd6211733878cc9aa17cf957723d88260e
Samantha H <hodge1214@gmail.com>
2022-11-12 12:47:41 -0500

    wrote constructor tests

--- app/src/test/java/com/example/librarysystem/AccountTest.java
+++ app/src/test/java/com/example/librarysystem/AccountTest.java
@@ -3,6 +3,7 @@ package com.example.librarysystem;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,19 +12,33 @@ public class AccountTest {
     public void correctDefaultConstructor(){
         Account a = new Account();
 
-        assertNull(a.userName);
-        assertNull(a.passWord);
-        assertFalse(a.isAdmin);
+        assertNull(a.userName, "The default constructor does not make the username null.");
+        assertNull(a.passWord, "The default constructor does not make the password null.");
+        assertFalse(a.isAdmin, "The default constructor does not set the admin boolean to false.");
     }
 
     @Test
     public void correctUserConstructorTest(){
+        Account a = new Account("Dave1234", "strongP@ssw0rd");
 
+        assertEquals("Dave1234", a.userName, "Your user constructor does not set the username to the given string.");
+        assertEquals("strongP@ssw0rd", a.passWord, "Your user constructor does not set the password to the given string.");
+        assertFalse(a.isAdmin, "Your user constructor does not set the admin boolean to false");
     }
 
     @Test
-    public void correctAdminConstructorTest(){
+    public void correctPotentialAdminConstructorTest(){
+        Account a = new Account("NotAnAdmin", "myCatsName", false);
+        Account b = new Account("IsAnAdmin", "he27dsb5", true);
 
+        assertEquals("NotAnAdmin", a.userName, "Your potential admin constructor does not set the username to the given string.");
+        assertEquals("IsAnAdmin", b.userName, "Your potential admin constructor does not set the username to the given string.");
+
+        assertEquals("myCatsName", a.passWord, "Your potential admin constructor does not set the password to the given string.");
+        assertEquals("he27dsb5", b.passWord, "Your potential admin constructor does not set the password to the given string.");
+
+        assertFalse(a.isAdmin, "Your potential admin constructor does not set isAdmin to false when passed a false boolean.");
+        assertTrue(b.isAdmin, "Your potential admin constructor does not set isAdmin to true when passed a true boolean.");
     }
 
     @Test
